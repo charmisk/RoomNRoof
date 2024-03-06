1560284ea779f6e0b85519a5eac067336457cab6
Kishan068 <ramanikishan02@gmail.com>
2024-03-06 17:44:56 +0530

    add pre installation

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -43,6 +43,7 @@ export const google = async (req, res, next) => {
         .cookie('access_token', token, { httpOnly: true })
         .status(200)
         .json(rest);
+        
     } else {
       const generatedPassword =
         Math.random().toString(36).slice(-8) +
